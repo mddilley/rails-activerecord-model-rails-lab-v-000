c63b651f3e813d7a53e9c7baf2b179079c7b2d1d
Mike Dilley <mike.dilley@gmail.com>
2019-01-09 22:01:37 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ActiveRecord::Base
 
-  def to_s(student)
-    student.first_name + " " + student.last_name    
+  def self.to_s
+    student.first_name + " " + student.last_name
   end
 
 end
